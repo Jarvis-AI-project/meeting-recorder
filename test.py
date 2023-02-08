175460d0bb7ddbe10b08351dbc8e5f425d533680
aarush_dwivedi <110493608+XEREXUS@users.noreply.github.com>
2023-02-08 22:49:28 +0530

    updated test.py

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ import time
 
 options = Options()
 options.add_argument('--profile-directory=Default')
-options.add_argument('--user-data-dir=/home/swarnim/.config/google-chrome/')
+options.add_argument('--user-data-dir=C:\\Users\\AARUSH\\AppData\\Local\\Google\\Chrome\\User Data\\Default')
 driver = uc.Chrome(options=options)
-driver.user_data_dir='/home/swarnim/.config/google-chrome/Default'
+driver.user_data_dir=r'C:\Users\AARUSH\AppData\Local\Google\Chrome\User Data\Default'
 driver.get('https://www.youtube.com/')
 time.sleep(50)
 driver.quit()
